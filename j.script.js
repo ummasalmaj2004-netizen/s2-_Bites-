ec23fce0b58a8c19b41cf04889350157b2e22e63
Ummasalma Jamil <ummasalmaj2004@gmail.com>
2026-07-02 05:57:02 +0800

    Add Firebase configuration and initialization
    Added Firebase configuration and initialization code.

--- j.script.js
+++ j.script.js
@@ -1,3 +1,18 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwcUC1vEAOQX3F6qbsLMRquByc1tHh33c",
+  authDomain: "square-bites.firebaseapp.com",
+  projectId: "square-bites",
+  storageBucket: "square-bites.firebasestorage.app",
+  messagingSenderId: "714058252261",
+  appId: "1:714058252261:web:9210d9b22f47fe3eb4790c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+
 const DELIVERY = 5;
 const cart = {};
 
