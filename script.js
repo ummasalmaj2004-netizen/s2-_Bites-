bf50a4af2bbfb50c23e6c5ad3ca7a09fb6b35149
Ummasalma Jamil <ummasalmaj2004@gmail.com>
2026-07-02 06:30:45 +0800

    Add Firebase auth state change listener

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+firebase.auth().onAuthStateChanged(user => {
+  if (!user) {
+    window.location.href = "index.html";
+  }
+});
+
 // ==========================
 // FIREBASE CONFIG
 // ==========================
